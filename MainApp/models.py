7519791c1afdf06e3bf9aecf5529e9d2f67d5ad1
Abby Brown <abby_brown3@baylor.edu>
2021-04-12 15:11:30 -0500

    -registering app

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Topic(models.Model):
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)   #Like "GetDate" function in SQL
+
+
+    def __str__(self):  #This way, when we call the class, it returns back the name and info we put in
+        return self.text
