f91dafed838671f9291feb0c4511470c64762cb0
Abby Brown <abby_brown3@baylor.edu>
2021-04-19 15:07:59 -0500

    uhhh

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 
@@ -6,3 +7,13 @@ from django.shortcuts import render
 def index(request):
     """The home page for learning_log"""
     return render(request, "MainApp/index.html")
+
+
+def topics(request):
+    topics = Topic.objects.order_by(
+        "data_added"
+    )  # Order by = ascending or descending sort order
+
+    context= {'topics':topics}
+    return render(request, "MainApp/topics.html",context)
+    #Context is an additional parameter passed to the index
