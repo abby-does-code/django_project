d7b25cc8474e43099f8a7370086b040122f1e508
Abby Brown <abby_brown3@baylor.edu>
2021-04-19 14:51:58 -0500

    Working in class

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -4,5 +4,11 @@ from . import views
 
 app_name = "MainApp"
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("topics", views.topics, name="topics"),
+]
 
+
+# URL points to view and view is the brain behind gathering info and displaying
+##Template as the bone and view as the neurons
