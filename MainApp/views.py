cb7c42f4f9b8649cc76144d231c4fc2f25ab7e21
Abby Brown <abby_brown3@baylor.edu>
2021-04-21 15:40:02 -0500

    new_entry

--- MainApp/views.py
+++ MainApp/views.py
@@ -42,3 +42,22 @@ def new_topic(request):
     # get request loads empty form
     # request.POST has all the info the user inputted on the website and adds to DB
     # is_valid() checks to make sure there's not errors on the form; if valid, save
+
+
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != "POST":  # load a new form
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+
+        if form.is_valid():
+            new_entry = form.save(commit=FALSE)
+            # with new entry, don't have associated topic
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect("MainApp:topic", topic_id=topic_id)
+            #  Page 'Topic' requires id; so we pull the topic id from the first line to tell it where to go
+    context = {'form':form,'topic':topic}
+    # pass object "topic" instead of id so it will show the topic and pass all attributes
+    return render(request, 'MainApp/new_entry.html',context)
