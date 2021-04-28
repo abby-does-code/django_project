5c4f574e2f37652af97b2ec820c44b09a9e58445
Abby Brown <abby_brown3@baylor.edu>
2021-04-28 14:36:32 -0500

    Move your apps to the top of "Installed Apps" thing in settings; this fixes log out

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -31,15 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My apps
+    "MainApp",
+    "users",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # My apps
-    "MainApp",
-    "users",
 ]
 
 MIDDLEWARE = [
